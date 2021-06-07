53ca3d0e17092e5c47dced9fe85c5ae038466fb6
kevin-guillen <kevin.guillen@globant.com>
2021-06-07 18:43:25 -0300

    - Added missing trailing commas - Added missing constants and deleted colors constants - Changed the class name of the dice to a more specific one. - Corrected the variable name DiceNumber to diceNumber

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,19 @@ import 'utils/constants.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Dice(),
+    home: DiceRoller(),
     debugShowCheckedModeBanner: false,
   ));
 }
 
-class Dice extends StatefulWidget {
+class DiceRoller extends StatefulWidget {
   // This widget is the root of your application.
   @override
   _DiceState createState() => _DiceState();
 }
 
-class _DiceState extends State<Dice> {
-  int DiceNumber = 1;
+class _DiceState extends State<DiceRoller> {
+  int diceNumber = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -27,45 +27,56 @@ class _DiceState extends State<Dice> {
           style: TextStyle(
             fontSize: Constants.appTitleFontSize,
             letterSpacing: Constants.appTitleLetterSpacing,
-            color: Constants.appTitleColor,
+            color: Colors.black,
             fontFamily: 'Play-Bold',
           ),
         ),
         flexibleSpace: Container(
           decoration: BoxDecoration(
-              gradient: LinearGradient(
-                  begin: Alignment.topLeft,
-                  end: Alignment.bottomRight,
-                  colors: <Color>[
-                Constants.titleBackgroundGradientStart,
-                Constants.titleBackgroundGradientEnd,
-              ])),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: <Color>[
+                Colors.purple,
+                Colors.blueAccent,
+              ],
+            ),
+          ),
         ),
       ),
       body: Container(
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/background/dicebackground3.jpg"),
-                fit: BoxFit.fill)),
+          image: DecorationImage(
+              image: AssetImage(
+                "assets/background/dicebackground3.jpg",
+              ),
+              fit: BoxFit.fill),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Expanded(
               child: Padding(
-                padding: const EdgeInsets.all(60.0),
-                child:
-                    Image.asset('assets/dice/dice$DiceNumber.png', width: 300),
+                padding: const EdgeInsets.all(
+                  Constants.ColumnElementsPadding,
+                ),
+                child: Image.asset(
+                  'assets/dice/dice$diceNumber.png',
+                  width: Constants.diceWidth,
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.only(bottom: 80.0),
+              padding: const EdgeInsets.only(
+                bottom: Constants.buttonBottomPadding,
+              ),
               child: ButtonTheme(
                 minWidth: Constants.buttonThemeMinWidth,
                 height: Constants.buttonThemeHeight,
-                child: RaisedButton(
+                child: ElevatedButton(
                   onPressed: () {
                     setState(() {
-                      DiceNumber = Random().nextInt(6) + 1;
+                      diceNumber = Random().nextInt(6) + 1;
                     });
                   },
                   child: Text(
@@ -73,7 +84,7 @@ class _DiceState extends State<Dice> {
                     style: TextStyle(
                       fontSize: Constants.playButtonFontSize,
                       letterSpacing: Constants.playButtonLetterSpacing,
-                      color: Constants.playButtonTextColor,
+                      color: Colors.black,
                       fontFamily: 'Play-Bold',
                     ),
                   ),
